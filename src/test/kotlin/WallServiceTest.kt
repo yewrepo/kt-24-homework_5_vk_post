e672a760561e4c9fdcd8ffc464540067c83b34c6
Dmitriy <Abramov.DV@delta.ru>
2021-11-08 11:06:38 +0300

    add Before annotation

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -4,13 +4,13 @@ import post.Post
 import post.PostType
 import java.util.*
 import org.junit.Assert.*
+import org.junit.Before
 import post.Comment
 
 internal class WallServiceTest {
 
     @Test
     fun createComment_shouldPass() {
-        resetArray()
         WallService.createComment(
             Comment(
                 postId = 1,
@@ -23,7 +23,6 @@ internal class WallServiceTest {
 
     @Test(expected = PostNotFoundException::class)
     fun createComment_shouldThrow() {
-        resetArray()
         WallService.createComment(
             Comment(
                 postId = 999,
@@ -36,13 +35,11 @@ internal class WallServiceTest {
 
     @Test
     fun add_postLastId() {
-        resetArray()
         assertEquals(3, WallService.lastId)
     }
 
     @Test
     fun update_shouldSuccess() {
-        resetArray()
         val update = Post(id = 2, date = Date().time, ownerId = 3, postType = PostType.Copy, text = "bla-bla")
         val result = WallService.update(update)
         assertTrue(result)
@@ -50,13 +47,13 @@ internal class WallServiceTest {
 
     @Test
     fun update_shouldFail() {
-        resetArray()
         val update = Post(id = 4545, date = Date().time, ownerId = 3, postType = PostType.Copy, text = "bla-bla")
         val result = WallService.update(update)
         assertFalse(result)
     }
 
-    private fun resetArray() {
+    @Before
+    fun resetArray() {
         WallService.clear()
         WallService.add(Post(date = Date().time, ownerId = 1, postType = PostType.Post, text = "Hello!"))
         WallService.add(Post(date = Date().time, ownerId = 2, postType = PostType.Post, text = "Hello World!"))
